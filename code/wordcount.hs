741cd62f0113cb0a28f050e679c47fb254a29b26
Emanuele Torelli <mennetorelli@hotmail.it>
2019-04-30 17:33:34 +0200

    Update wordcount.hs

--- code/wordcount.hs
+++ code/wordcount.hs
@@ -2,27 +2,39 @@ import Conduit
 import Control.Monad
 import Data.Char (isAlphaNum)
 import Data.HashMap.Strict (empty, insertWith, toList)
-import Data.List.Split
-import Data.Text (pack, unpack, toLower)
+import Data.List.Split (splitOn)
+import Data.Text (pack, unpack, toLower, splitOn, filter, words)
 import System.IO
 
-standard :: IO ()
-standard = do
+wordcount :: IO ()
+wordcount = do
     withFile "input.txt" ReadMode $ \handle -> do
         content <- hGetContents handle
         putStrLn $ show $ foldr
             (\x v -> insertWith (+) x 1 v) 
             empty 
-            (fmap (unpack . toLower . pack)
-                $ fmap (filter isAlphaNum)
-                $ splitOn " " content)
+            (fmap (unpack . Data.Text.toLower . pack)
+                $ fmap (Prelude.filter isAlphaNum)
+                $ Data.List.Split.splitOn " " content)
 
+wordcountC :: IO ()
+wordcountC = do
+    text <- runConduitRes $ sourceFile "input.txt"
+        .| decodeUtf8C
+        .| foldC
+    count <- runConduit $ yieldMany (Data.Text.words text)
+        .| mapC (Data.Text.filter isAlphaNum)
+        .| mapC (Data.Text.toLower)
+        .| sinkList
+    print count
 
 
 main :: IO ()
 main = do
-    putStrLn "1: standard wordcount without Conduit"
+    putStrLn "1: wordcount without Conduit"
+    putStrLn "2: wordcount with Conduit"
     choice <- getLine
     case choice of
-        "1" -> standard
+        "1" -> wordcount
+        "2" -> wordcountC
         _ -> main
