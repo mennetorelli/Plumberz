8f706400721cfcbb5f126bddd06a7b7cb31d52e6
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-11 17:42:12 +0200

    client e server WIP

--- code/client.hs
+++ code/client.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Conduit
-import Control.Concurrent.Async (concurrently)
 import Control.Monad (void)
 import Data.Conduit.Network
+import Data.Word8 (toLower, isAlphaNum)
 
 main :: IO ()
-main =
-    runTCPClient (clientSettings 4000 "localhost") $ \server ->
-        void $ concurrently
-            (runConduit $ stdinC .| appSink server)
-            (runConduit $ appSource server .| stdoutC)
+main = runTCPClient (clientSettings 4000 "localhost") $ \server ->
+    runConduitRes $ sourceFile "input.txt"
+    .| omapCE toLower
+    .| peekForeverE (do
+        word <- takeWhileCE isAlphaNum
+        dropCE 1
+        return word)
+    .| appSink server
+
--- code/server.hs
+++ code/server.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Conduit
 import Data.Conduit.Network
-import Data.Word8 (toUpper)
+import Data.HashMap.Strict (empty, insertWith, toList)
 
 main :: IO ()
-main = runTCPServer (serverSettings 4000 "*") $ \appData ->
-    runConduit $ appSource appData 
-        .| omapCE toUpper
-        .| appSink appData
+main = runTCPServer (serverSettings 4000 "*") $ \appData -> do
+    hashMap <- runConduit $ appSource appData 
+        .| foldMC insertInHashMap empty
+    print (toList hashMap)
+
+insertInHashMap x v = do
+    return (insertWith (+) v 1 x)
