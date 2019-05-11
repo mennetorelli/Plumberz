4d04200a5c3f60b51f20d5fcab91f8d3e26ae9d7
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-11 18:06:27 +0200

    Update client.hs

--- code/client.hs
+++ code/client.hs
@@ -4,8 +4,9 @@ import Control.Monad (void)
 import Data.Conduit.Network
 import Data.Word8 (toLower, isAlphaNum)
 
-main :: IO ()
-main = runTCPClient (clientSettings 4000 "localhost") $ \server ->
+
+client_file :: IO ()
+client_file = runTCPClient (clientSettings 4000 "localhost") $ \server ->
     runConduitRes $ sourceFile "input.txt"
     .| omapCE toLower
     .| peekForeverE (do
@@ -13,4 +14,30 @@ main = runTCPClient (clientSettings 4000 "localhost") $ \server ->
         dropCE 1
         return word)
     .| appSink server
+
+
+client_stdin :: IO ()
+client_stdin = runTCPClient (clientSettings 4000 "localhost") $ \server ->
+    runConduit $ stdinC
+    .| omapCE toLower
+    .| peekForeverE (do
+        word <- takeWhileCE isAlphaNum
+        dropCE 1
+        return word)
+    .| appSink server
+
+
+
+main :: IO ()
+main = do
+    putStrLn "1: input from file"
+    putStrLn "2: input from stdin"
+    choice <- getLine
+    case choice of
+        "1" -> do 
+            client_file
+        "2" -> do
+            client_stdin
+
+    
 
