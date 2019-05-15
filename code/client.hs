a6b008e7a984454cce017def107446c533890f45
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-15 11:33:18 +0200

    Update client.hs

--- code/client.hs
+++ code/client.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Conduit
 import Control.Monad (void)
+import qualified Data.Conduit.Combinators as CC
 import Data.Conduit.Network
 import Data.Word8 (toLower, isAlphaNum)
 
@@ -9,10 +10,7 @@ client_file :: IO ()
 client_file = runTCPClient (clientSettings 4000 "localhost") $ \server ->
     runConduitRes $ sourceFile "input.txt"
     .| omapCE toLower
-    .| peekForeverE (do
-        word <- takeWhileCE isAlphaNum
-        dropCE 1
-        return word)
+    .| CC.splitOnUnboundedE (not . isAlphaNum)
     .| appSink server
 
 
@@ -20,10 +18,7 @@ client_stdin :: IO ()
 client_stdin = runTCPClient (clientSettings 4000 "localhost") $ \server ->
     runConduit $ stdinC
     .| omapCE toLower
-    .| peekForeverE (do
-        word <- takeWhileCE isAlphaNum
-        dropCE 1
-        return word)
+    .| CC.splitOnUnboundedE (not . isAlphaNum)
     .| appSink server
 
 
@@ -35,10 +30,14 @@ main = do
     putStrLn "3: exit"
     choice <- getLine
     case choice of
-        "1" -> client_file
-        "2" -> client_stdin
+        "1" -> do
+            client_file
+            main
+        "2" -> do
+            client_stdin
+            main
         "3" -> return ()
-    main
+
 
     
 
