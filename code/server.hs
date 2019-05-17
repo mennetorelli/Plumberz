18d066983b7ea7b76ebdc0d3922750537a879b85
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-17 15:50:16 +0200

    rimosso server_timewindow.hs

--- code/server.hs
+++ code/server.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad (forever)
+import Data.Functor (void)
+
 import Data.ByteString.Char8 (pack)
-import Data.Word8 (toLower, isAlphaNum)
 import Data.HashMap.Strict (empty, insertWith, toList)
+import Data.Word8 (toLower, isAlphaNum)
 
 import Conduit
 import qualified Data.Conduit.Combinators as CC
 import Data.Conduit.Network
 
+import Control.Concurrent (threadDelay, takeMVar, putMVar, newMVar)
+import Control.Concurrent.Async
+import System.Timeout
 
-main :: IO ()
-main = runTCPServer (serverSettings 4000 "*") $ \appData -> do
+
+server :: IO ()
+server = runTCPServer (serverSettings 4000 "*") $ \appData -> do
     hashMap <- runConduit $ appSource appData 
         .| omapCE toLower
         .| CC.splitOnUnboundedE (not . isAlphaNum)
@@ -21,3 +28,33 @@ main = runTCPServer (serverSettings 4000 "*") $ \appData -> do
 
 insertInHashMap x v = do
     return (insertWith (+) v 1 x)
+
+
+server_tw :: Int -> IO ()
+server_tw timeWindow = runTCPServer (serverSettings 4000 "*") $ \appData -> do
+    hashMapMVar <- newMVar empty
+    void $ concurrently
+        (do 
+            hashMap <- runConduit $ appSource appData 
+                .| omapCE toLower
+                .| CC.splitOnUnboundedE (not . isAlphaNum)
+                .| foldMC insertInHashMap empty
+            putMVar hashMapMVar hashMap)
+        (forever $ do 
+            threadDelay timeWindow
+            hashMap <- takeMVar hashMapMVar
+            runConduit $ yield (pack $ show $ toList hashMap)
+                .| appSink appData)
+    
+
+main :: IO ()
+main = do
+    putStrLn "Server without time window"
+    putStrLn "Server with time window"
+    choice <- getLine
+    case choice of
+        "1" -> server
+        "2" -> do
+            putStrLn "Insert time window (in seconds)"
+            timeWindow <- getLine
+            server_tw $ (read timeWindow) * 1000000
