7b1fb4aad27c56a889e2953889f99359a56fb646
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-17 13:53:43 +0200

    Update server_timewindow.hs

--- code/server_timewindow.hs
+++ code/server_timewindow.hs
@@ -1,20 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (forever)
+import Data.Functor (void)
+
+import Data.ByteString.Char8 (pack)
+import Data.HashMap.Strict (empty, insertWith, toList)
+import Data.Word8 (toLower, isAlphaNum)
+
 import Conduit
+import qualified Data.Conduit.Combinators as CC
+import Data.Conduit.Network
+
 import Control.Concurrent (forkOS, threadDelay, takeMVar, putMVar, newMVar)
 import Control.Concurrent.Async
-import Control.Monad (forever)
-import Data.Conduit.Network
-import Data.HashMap.Strict (empty, insertWith, toList)
 import System.Timeout
 
-main :: IO ()
-main = runTCPServer (serverSettings 4000 "*") $ \appData -> do
+
+main2 :: IO ()
+main2 = runTCPServer (serverSettings 4000 "*") $ \appData -> do
     hashMap <- runConduit $ appSource appData 
         .| timedWindow 5000000
     return ()
 
-insertInHashMap x v = do
-    return (insertWith (+) v 1 x)
+
+main :: IO ()
+main = runTCPServer (serverSettings 4000 "*") $ \appData -> do
+    hashMapMVar <- newMVar empty
+    void $ concurrently
+        (do 
+            hashMap <- runConduit $ appSource appData 
+                .| omapCE toLower
+                .| CC.splitOnUnboundedE (not . isAlphaNum)
+                .| foldMC insertInHashMap empty
+            putMVar hashMapMVar hashMap)
+        (forever $ do 
+            threadDelay 5000000
+            hashMap <- takeMVar hashMapMVar
+            print $ toList hashMap)
+            -- runConduit $ yield (pack $ show $ toList hashMap)
+            --    .| iterMC print)
+            --    .| appSink appData)
+
 
 timedWindow t = do
     v <- lift $ newMVar empty
@@ -33,3 +59,7 @@ wordcount' e v = do
     s' <- lift $ takeMVar v
     lift $ putMVar v $ insertWith (+) x 1 s'
     wordcount' e v
+
+
+insertInHashMap x v = do
+    return (insertWith (+) v 1 x)
