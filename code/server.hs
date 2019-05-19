d338ecff3c5e9f3d229864ab73d3b04600b02d67
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-19 23:45:55 +0200

    Update server.hs

--- code/server.hs
+++ code/server.hs
@@ -4,14 +4,15 @@ import Control.Monad (forever)
 import Data.Functor (void)
 
 import Data.ByteString.Char8 (pack)
-import Data.HashMap.Strict (empty, insertWith, toList)
+import Data.Maybe (fromJust)
+import Data.HashMap.Strict (empty, insertWith, toList, unionWith)
 import Data.Word8 (toLower, isAlphaNum, _percent)
 
 import Conduit
 import qualified Data.Conduit.Combinators as CC
 import Data.Conduit.Network
 
-import Control.Concurrent (threadDelay, takeMVar, putMVar, newMVar)
+import Control.Concurrent (threadDelay, takeMVar, putMVar, newMVar, tryTakeMVar)
 import Control.Concurrent.Async
 import System.Timeout
 
@@ -37,33 +38,17 @@ server_tw timeWindow = runTCPServer (serverSettings 4000 "*") $ \appData -> do
                 .| omapCE toLower
                 .| CC.splitOnUnboundedE (not . isAlphaNum)
                 .| foldMC insertInHashMap empty
-            putMVar hashMapMVar hashMap)
+            queuedHashMap <- tryTakeMVar hashMapMVar
+            putMVar hashMapMVar $ unionWith (+) hashMap (if queuedHashMap == Nothing 
+                                                            then empty 
+                                                            else fromJust queuedHashMap))
         (forever $ do 
             threadDelay timeWindow
-            hashMap <- takeMVar hashMapMVar
-            runConduit $ yield (pack $ show $ toList hashMap)
+            hashMap <- tryTakeMVar hashMapMVar
+            runConduit $ yield (pack $ show $ toList (if hashMap == Nothing 
+                                                            then empty 
+                                                            else fromJust hashMap))
                 .| appSink appData)
-
-
-{-server_tw2 :: Int -> IO ()
-server_tw2 timeWindow = runTCPServer (serverSettings 4000 "*") $ \appData -> do
-    timeOut <- newMVar False
-    void $ concurrently
-        (do 
-            hashMap <- runConduit $ appSource appData 
-                .| omapCE toLower
-                .| CC.splitOnUnboundedE (not . isAlphaNum)
-                .| do
-                    timeOutValue <- takeMVar timeOut
-                    if timeOutValue == True
-                        then foldMC insertInHashMap empty
-                        else lift $ return ()
-            runConduit $ yield (pack $ show $ toList hashMap)
-                .| iterMC print
-                .| appSink appData)
-        (forever $ do 
-            threadDelay timeWindow
-            putMVar timeOut True)-}
 
 
 insertInHashMap x v = do
