2e91831eca53428020b38a09d2a51a7ce6cbd2f1
Emanuele Torelli <mennetorelli@hotmail.it>
2019-05-30 13:18:55 +0200

    Update server.hs

--- code/server.hs
+++ code/server.hs
@@ -22,7 +22,9 @@ server = runTCPServer (serverSettings 4000 "*") $ \appData -> forever $ do
     hashMap <- runConduit $ appSource appData 
         .| takeWhileCE (/= _percent)
         .| omapCE toLower
-        .| CC.splitOnUnboundedE (not . isAlphaNum)
+        .| do 
+            CC.splitOnUnboundedE (not . isAlphaNum)
+            dropWhileC (/= "")
         .| foldMC insertInHashMap empty
     runConduit $ yield (pack $ show $ toList hashMap)
         .| appSink appData
@@ -36,7 +38,9 @@ server_tw timeWindow = runTCPServer (serverSettings 4000 "*") $ \appData -> do
             hashMap <- runConduit $ appSource appData
                 .| takeWhileCE (/= _percent)
                 .| omapCE toLower
-                .| CC.splitOnUnboundedE (not . isAlphaNum)
+                .| do 
+                    CC.splitOnUnboundedE (not . isAlphaNum)
+                    dropWhileC (/= "")
                 .| foldMC insertInHashMap empty
             queuedHashMap <- tryTakeMVar hashMapMVar
             putMVar hashMapMVar $ unionWith (+) hashMap (extractHashMap queuedHashMap))
